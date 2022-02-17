c623b0a6d3d89894a3825405f8459b59be175d94
Sean McBane <sean.mcbane@protonmail.com>
2022-02-17 14:04:26 -0600

    Add a single header to collect functionality

---
+++ stokhos.hpp
@@ -0,0 +1,9 @@
+#ifndef STOKHOS_HEADER_INCLUDED
+#define STOKHOS_HEADER_INCLUDED
+
+#include "splitmix64.hpp"
+#include "truncated_generator.hpp"
+#include "xoshiro256starstar.hpp"
+
+#endif // STOKHOS_HEADER_INCLUDED
+
